33f36b820b36ddc228e02cf68aa0c416ee5d8926
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-18 14:54:47 +0200

    (feat) rendre l'option return fonctionnel

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 package src;
 
-
+import java.util.Scanner;
 import src.TerminalDisplay.Display;
 
 public class Main {
 
     public static void main(String[] args) {
-        Display.displayWelcomeMessage();
-        String inputString = Display.getInputString();
-        Display.displayStringToTranslate(inputString);
-        String inputBase = Display.getTranslationBase();
-        Display.displayTranslationBase(inputBase);
-       
+
+        Scanner input = new Scanner(System.in);
+        try {
+            String statusBase;
+            do {
+                statusBase = Display.startTranslation(input);
+            } while (statusBase.equals("r") || statusBase.equals("return"));
+        } finally {
+            input.close();
+        }
+
     }
 }
 
--- src/TerminalDisplay.java
+++ src/TerminalDisplay.java
@@ -1,77 +1,79 @@
 package src;
+
 import java.util.Scanner;
 
 import src.InputUser.InputValidator;
 
 public class TerminalDisplay {
 
-    public class Display{   
-    
-    // Welcome Message
-    public static void displayWelcomeMessage() {
-        System.out.print("\n" +
-            "       ╔═══════════════════════════════════════════╗\n" +
-            "       ║                                           ║\n" +
-            "       ║       WELCOME TO GLOBAL CONVERTER         ║\n" +
-            "       ║        By Lucas M., Than L, Vanny L.      ║\n" +
-            "       ║                                           ║\n" +
-            "       ╚═══════════════════════════════════════════╝\n"
-        );
-    }
+    public class Display {
 
-    // Ask user to enter a string to translate and check if the input is valid
-    public static String getInputString() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("\nPlease enter a string to translate: ");
-        String inputString = input.nextLine();
-        System.out.println();        
-
-        while (!InputValidator.isValidString(inputString)) {
-            System.out.print("Invalid input! Please enter a valid string without special characters." +
-                             "\nPlease enter a string to translate: ");
-            inputString = input.nextLine();
-            System.out.println();
+        // Welcome Message
+        public static void displayWelcomeMessage() {
+            System.out.print("\n" +
+                    "       ╔═══════════════════════════════════════════╗\n" +
+                    "       ║                                           ║\n" +
+                    "       ║       WELCOME TO GLOBAL CONVERTER         ║\n" +
+                    "       ║        By Lucas M., Than L, Vanny L.      ║\n" +
+                    "       ║                                           ║\n" +
+                    "       ╚═══════════════════════════════════════════╝\n");
         }
 
-        input.close();
-        return inputString;
-    }
-
-    // Display String
-    public static void displayStringToTranslate(String inputString) {
-        System.out.printf("Here is the string you want to translate: %s\n\n", inputString);
-    }
-
-
-    public static String getTranslationBase() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("\n" +
-            "       ╔═══════════════════════════════════════════╗\n" +
-            "       ║      Available Translation bases :        ║\n" +
-            "       ║                                           ║\n" +
-            "       ║      Text   (t)      Hexadecimal (h)      ║\n" +
-            "       ║      Octal  (o)      Decimal     (d)      ║\n" +
-            "       ║      Binary (b)      Return      (r)      ║\n" +
-            "       ╚═══════════════════════════════════════════╝\n" +
-            "\nPlease enter your translation base: "
-        );
-
-        String inputBase = input.nextLine().toLowerCase();
-
-        while (!InputValidator.isValidBase(inputBase)) {
-            System.out.print("Invalid input! Please choose one of the available base options" +
-                             "\nPlease enter your translation base: ");
-            inputBase = input.nextLine().toLowerCase();
+        // Ask user to enter a string to translate and check if the input is valid
+        public static String getInputString(Scanner input) {
+            System.out.print("\nPlease enter a string to translate: ");
+            String inputString = input.nextLine();
             System.out.println();
+
+            while (!InputValidator.isValidString(inputString)) {
+                System.out.print("Invalid input! Please enter a valid string without special characters." +
+                        "\nPlease enter a string to translate: ");
+                inputString = input.nextLine();
+                System.out.println();
+            }
+            return inputString;
         }
-        input.close();
-        return inputBase;
+
+        // Display String
+        public static void displayStringToTranslate(String inputString) {
+            System.out.println();
+            System.out.printf("Here is the string you want to translate: %s\n\n", inputString);
+        }
+
+        public static String getTranslationBase(Scanner input) {
+            System.out.print("\n" +
+                    "       ╔═══════════════════════════════════════════╗\n" +
+                    "       ║      Available Translation bases :        ║\n" +
+                    "       ║                                           ║\n" +
+                    "       ║      Text   (t)      Hexadecimal (h)      ║\n" +
+                    "       ║      Octal  (o)      Decimal     (d)      ║\n" +
+                    "       ║      Binary (b)      Return      (r)      ║\n" +
+                    "       ╚═══════════════════════════════════════════╝\n" +
+                    "\nPlease enter your translation base: ");
+
+            String inputBase = input.nextLine().toLowerCase();
+
+            while (!InputValidator.isValidBase(inputBase)) {
+                System.out.print("Invalid input! Please choose one of the available base options" +
+                        "\nPlease enter your translation base: ");
+                inputBase = input.nextLine().toLowerCase();
+                System.out.println();
+
+            }
+            return inputBase;
+        }
+
+        public static void displayTranslationBase(String inputBase) {
+            System.out.printf("Here is the translation base you want: %s\n\n", inputBase);
+        }
+
+        public static String startTranslation(Scanner input) {
+            Display.displayWelcomeMessage();
+            String inputString = getInputString(input);
+            displayStringToTranslate(inputString);
+            String inputBase = Display.getTranslationBase(input);
+            Display.displayTranslationBase(inputBase);
+            return inputBase;
+        }
     }
-
-    public static void displayTranslationBase(String inputBase) {
-        System.out.printf("Here is the translation base you want: %s\n\n", inputBase);
-    }
-
-}}
-
-
+}
