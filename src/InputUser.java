2fdbfcaa283e78626cb66ee91a28fd8b94a12d62
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-18 11:40:37 +0200

    (feat) Create rect to add title and names

--- src/InputUser.java
+++ src/InputUser.java
@@ -1,9 +1,17 @@
 package src;
 public class InputUser {
+    public class InputValidator {
+        public static boolean isValidString(String input) {
 
-    public static void main(String[] args) {
-   
-
+            // Verify if input contains only alphabets and/or numerics
+            return input.matches("[a-zA-Z0-9]+");
+        }
+    
+        public static boolean isValidBase(String base) {
+            // Validate base input
+            return base.matches("text|t|hexadecimal|h|octal|o|decimal|d|binary|b");
+        }
     }
     
-}
+    }
+    
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 package src;
-public class Main {
+
+import java.util.Scanner;
+import src.InputUser;
+
+
+public class Main extends InputUser {
     public static void main(String[] args) {
+
         
+        // Ask the user to enter a string to translate
+        Scanner input = new Scanner(System.in); 
+        System.out.print("\n" +
+            "       ╔═══════════════════════════════════════════╗\n" +
+            "       ║                                           ║\n" +
+            "       ║       WELCOME TO GLOBAL CONVERTER         ║\n" +
+            "       ║        By Lucas M., Than L, Vanny L.      ║\n" +
+            "       ║                                           ║\n" +
+            "       ╚═══════════════════════════════════════════╝\n" +
+            "\n          Please enter a string to translate:" 
+        );    
+        
+        String input1 = input.nextLine();
+        
+        System.out.println(input1);
+        input.close();        
     }
 }
