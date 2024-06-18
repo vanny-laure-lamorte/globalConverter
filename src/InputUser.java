7008cd159d26955fa222e751503bdd06f22ce86a
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-06-18 14:23:37 +0200

    (feat) create TerminalDisplay class

--- src/InputUser.java
+++ src/InputUser.java
@@ -3,13 +3,14 @@ public class InputUser {
     public class InputValidator {
         public static boolean isValidString(String input) {
 
-            // Verify if input contains only alphabets and/or numerics
-            return input.matches("[a-zA-Z0-9]+");
+            // Verify if input contains only alphabets / numerics / space
+            return input.matches("[a-zA-Z0-9 ]+");
         }
     
         public static boolean isValidBase(String base) {
             // Validate base input
-            return base.matches("text|t|hexadecimal|h|octal|o|decimal|d|binary|b");
+            return base.matches("text|t|hexadecimal|h|octal|o|decimal|d|binary|b|return|r");
+         //   return base.matches("text|Text|t|hexadecimal|Hexadecimal|h|octal|Octal|o|decimal|Decimal|d|binary|Binary|b|return|Return|r");
         }
     }
     
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,18 @@
 package src;
 
-import java.util.Scanner;
 
-public class Main extends InputUser {
+import src.TerminalDisplay.Display;
+
+public class Main {
+
     public static void main(String[] args) {
-
-        
-        // Ask the user to enter a string to translate
-        Scanner input = new Scanner(System.in); 
-        System.out.print("\n" +
-            "       ╔═══════════════════════════════════════════╗\n" +
-            "       ║                                           ║\n" +
-            "       ║       WELCOME TO GLOBAL CONVERTER         ║\n" +
-            "       ║        By Lucas M., Than L, Vanny L.      ║\n" +
-            "       ║                                           ║\n" +
-            "       ╚═══════════════════════════════════════════╝\n" +
-            "\nPlease enter a string to translate: " 
-        );
-        
-        String input1 = input.nextLine();       
-        
-        // Verify if the user enter a valid input
-        while (!InputValidator.isValidString(input1)) {
-            System.out.println("Invalid input! Please enter a valid string.");
-           input1 = input.nextLine();
-        }; 
-
-
-        
-        input.close();  
-        
-        
+        Display.displayWelcomeMessage();
+        String inputString = Display.getInputString();
+        Display.displayStringToTranslate(inputString);
+        String inputBase = Display.getTranslationBase();
+        Display.displayTranslationBase(inputBase);
+       
     }
 }
+
+
